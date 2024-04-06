a9eb0caebeb83d3541948e359707f0b87b9bffe6
erosaddy <zxz7648@gmail.com>
2024-04-06 15:10:03 +0900

    added sign in / sign up

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+const modal = document.querySelector('#modal');
+const btnSignInModal = document.querySelector('.btn-signin-modal');
+const btnSignUpModal = document.querySelector('.btn-signup-modal');
+const btnCloseModal = document.querySelector('.modal-close')
+
+// 로그인 버튼을 눌러 로그인 모달 띄우기
+btnSignInModal.addEventListener("click", ()=>{
+    modal.style.display="flex";
+    $(".email-signup").hide();
+    $(".email-login").delay(1).fadeIn(1);
+    $(".email-signup").fadeOut(1);
+    $("#login-box-link").addClass("active");
+    $("#signup-box-link").removeClass("active");
+});
+// 회원가입 버튼을 눌러 회원가입 모달 띄우기
+btnSignUpModal.addEventListener("click", ()=>{
+    modal.style.display="flex";
+    $(".email-login").hide();
+    $(".email-login").fadeOut(1);
+    $(".email-signup").delay(1).fadeIn(1);
+    $("#login-box-link").removeClass("active");
+    $("#signup-box-link").addClass("active");
+});
+// X 버튼을 눌러 모달을 탈출
+btnCloseModal.addEventListener("click", ()=>{
+    modal.style.display="none";
+});
